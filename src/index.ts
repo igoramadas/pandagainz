47e8a736ddb07484bbc04d7692350f4043a3065a
Igor Ramadas <igor@devv.com>
2020-08-04 20:00:50 +0200

    Load port from env variable.

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,27 @@ if (!process.env.NODE_ENV) {
     process.env.NODE_ENV = "development"
 }
 
+// Start logger.
 import logger = require("anyhow")
 logger.setup("console")
 logger.levelOnConsole = true
 logger.uncaughtExceptions = true
 logger.unhandledRejections = true
 
+// Load settings.
 import setmeup = require("setmeup")
 setmeup.load()
 setmeup.load("settings.private.json")
 
+// Port set via the PORT env?
+if (process.env.PORT) {
+    setmeup.settings.app.port = process.env.PORT
+}
+
+// Init Express server.
 import expresser = require("expresser")
 expresser.app.init()
 
+// Load routes.
 import routes = require("./routes")
 routes.init()
